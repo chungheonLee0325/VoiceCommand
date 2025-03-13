49a5fd54d927bc8ed03917bcc98dbc41042e673a
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-03-13 10:30:14 +0900

    feat: add ue proj

---
+++ ProjVoiceCommand/Source/ProjVoiceCommand/ProjVoiceCommandGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjVoiceCommandGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjVoiceCommandGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjVoiceCommandGameMode();
+};
+
+
+
