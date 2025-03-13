02c760e05a18ee3409490927561ee2a7db8ec70d
김형후 <kimjinsyll@gmail.com>
2025-03-13 14:13:46 +0900

    create_module

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import os
 from google.cloud import speech
 from openai import OpenAI
 from dotenv import load_dotenv
+from module import transcribe_audio, generate_unreal_code
 
 app = FastAPI()
 
@@ -15,34 +16,6 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 @app.get("/")
 def home():
     return {"message": "안녕하살법! 하지메 마시떼!"}
-
-# def transcribe_audio(file_path: str) -> str:
-#     with open(file_path, "rb") as audio_file:
-#         content = audio_file.read()
-#     audio = speech.RecognitionAudio(content=content)
-#     config = speech.RecognitionConfig(
-#         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-#         sample_rate_hertz=16000,
-#         language_code="en-US"
-#     )
-#     response = client.recognize(config=config, audio=audio)
-#     texts = [result.alternatives[0].transcript for result in response.results]
-#     return " ".join(texts) if texts else ""
-
-# def generate_unreal_code(text: str) -> str:
-#     url = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateText?key={GEMINI_API_KEY}"
-#     headers = {"Content-Type": "application/json"}
-#     data = {
-#         "prompt": { #프롬프트 수정
-#             "text": f"Convert the following description into Unreal Engine C++ code:\n{text}"
-#         },
-#         "temperature": 0.7
-#     }
-#     response = requests.post(url, json=data, headers=headers)
-#     if response.status_code == 200:
-#         return response.json().get("candidates")[0]["output"]
-#     else:
-#         raise HTTPException(status_code=500, detail="Failed to generate Unreal code")
 
 
 # @app.post("/convert-audio")
