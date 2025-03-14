fbb73d6cbe72127415737bc6e55b2c909bf8fc85
choiyeontaek <dusxorchl@naver.com>
2025-03-14 09:40:34 +0900

    통신C++ 1. 메인 캐릭터인 손해임 캐릭터로 코드 이동 2. 기존 테스트 통신 캐릭터 통신코드 전부 주석처리 3. 메인 캐릭터 Middle Map 에 월드세팅 ( 게임모드 , 플레이어 ) 설정 완료 4. 손해임 캐릭터 캡쳐 컴포넌트 재생성 및 저장

--- Sonheim/Source/Sonheim/AreaObject/Player/SonheimPlayer.cpp
+++ Sonheim/Source/Sonheim/AreaObject/Player/SonheimPlayer.cpp
@@ -8,6 +8,7 @@
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "http.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Sonheim/Animation/Player/PlayerAniminstance.h"
 #include "Sonheim/AreaObject/Skill/Base/BaseSkill.h"
@@ -141,6 +142,32 @@ void ASonheimPlayer::Reward(int ItemID, int ItemValue) const
 void ASonheimPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (GetWorld()->GetFirstPlayerController()->WasInputKeyJustPressed(EKeys::T))
+	{
+		SendWavFileAsJson(); 
+		// 'T' 키가 눌렸을 때 WAV 파일을 JSON으로 전송하는 함수 호출
+	}
+}
+
+void ASonheimPlayer::SendWavFileAsJson()
+{
+	FString FilePath = FPaths::ProjectSavedDir() + TEXT("C:/AIPW/Sonheim/Saved/BouncedWavFiles/Test.wav");
+	// 저장된 WAV 파일의 경로를 지정 (Saved 폴더 내 Test.wav)
+
+	TArray<uint8> BinaryData;
+	LoadWavFile(FilePath, BinaryData);
+	// 지정된 경로에서 WAV 파일을 로드하고 바이너리 데이터 배열에 저장
+
+	FString Base64EncodedData;
+	EncodeToBase64(BinaryData, Base64EncodedData);
+	// 로드된 바이너리 데이터를 Base64 형식으로 인코딩
+
+	FString JsonString = FString::Printf(TEXT("{\"audio\":\"%s\"}"), *Base64EncodedData);
+	// Base64 인코딩된 데이터를 JSON 문자열로 변환 (키: audio)
+
+	SendJsonData(JsonString);
+	// JSON 데이터를 서버로 전송
 }
 
 void ASonheimPlayer::InitializeStateRestrictions()
@@ -195,6 +222,57 @@ bool ASonheimPlayer::CanPerformAction(EPlayerState State, FString ActionName)
 		return Restrictions.bCanAction;
 
 	return false;
+}
+
+void ASonheimPlayer::LoadWavFile(const FString& FilePath, TArray<uint8>& BinaryData)
+{
+	if (!FFileHelper::LoadFileToArray(BinaryData, *FilePath))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to load WAV file from path: %s"), *FilePath);
+		return;
+		// 지정된 경로에서 WAV 파일을 로드실패 
+	}
+}
+
+void ASonheimPlayer::EncodeToBase64(const TArray<uint8>& BinaryData, FString& Base64EncodedData)
+{
+	Base64EncodedData = FBase64::Encode(BinaryData);
+	// 바이너리 데이터를 Base64 형식으로 인코딩하여 문자열에 저장
+}
+
+void ASonheimPlayer::SendJsonData(const FString& JsonString)
+{
+	FHttpRequestRef Request = FHttpModule::Get().CreateRequest();
+	// HTTP 요청 객체 생성
+
+	Request->SetURL(TEXT("http://192.168.20.53:8000/convert-audio"));
+	Request->SetURL(TEXT("http://192.168.20.53:72/convert-audio"));
+	// 요청할 서버의 URL 설정
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! AI 분들께 아침에 받으면 넣어서 테스트 꼭 해보기!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	
+	Request->SetVerb(TEXT("POST"));
+	// HTTP 요청 방식을 POST로 설정
+
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	// 요청 헤더에 Content-Type을 application/json으로 설정
+
+	Request->SetContentAsString(JsonString);
+	// 요청 본문에 JSON 문자열 추가
+
+	Request->OnProcessRequestComplete().BindLambda(
+		[](FHttpRequestPtr Req, FHttpResponsePtr Res, bool bWasSuccessful)
+		{
+			if (bWasSuccessful && Res.IsValid())
+			{
+				UE_LOG(LogTemp, Log, TEXT("Successfully sent JSON data! Response: %s"), *Res->GetContentAsString());
+				// 요청이 성공
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("Failed to send JSON data."));
+				// 요청이 실패
+			}
+		});
 }
 
 void ASonheimPlayer::SetComboState(bool bCanCombo, int SkillID)
